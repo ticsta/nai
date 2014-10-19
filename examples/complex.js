a4a2b7623d62096cf3e19aad750ef22ba1ff0549
Dumitru Cantea <canteadumitru@gmail.com>
2014-10-19 13:45:26 +0300

    fix node children

---
+++ examples/complex.js
@@ -0,0 +1,17 @@
+var nai = require('../lib/nai'),
+  Promise = require('bluebird');
+
+nai.renderFile(__dirname+'/complex.html', {
+  locals: {
+    getUsers: function() {
+      return new Promise(function(resolve) {
+        setTimeout(function() {
+          resolve("fraier 10!!!");
+        }, 1);
+      });
+    }
+  }
+})
+  .then(function(result) {
+    console.log(result);
+  });
