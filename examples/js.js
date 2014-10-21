c88cda601142cc78a3d841f14654c49844187432
Dumitru Cantea <canteadumitru@gmail.com>
2014-10-21 21:44:05 +0300

    fixed js ' escape 0.0.3

---
+++ examples/js.js
@@ -0,0 +1,14 @@
+var nai = require('../lib/nai'),
+  Promise = require('bluebird');
+
+
+nai.renderFile(__dirname + '/js.html', {
+  locals: {
+    document: {
+      id: 12
+    }
+  }
+})
+  .then(function(result) {
+    console.log(result);
+  });
