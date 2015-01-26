bf701185012bc13640676fbf10da41c6e538416e
Dumitru Cantea <canteadumitru@gmail.com>
2015-01-26 17:58:18 +0200

    hierarchy example, WOKING!

---
+++ examples/hierarchy.js
@@ -0,0 +1,37 @@
+var nai = require('../lib/nai'),
+  Promise = require('bluebird'),
+  data = {
+    first: {
+      name: 'First'
+    },
+    second: {
+      name: 'Second'
+    },
+    _3rd: {
+      name: '3rd'
+    }
+  };
+
+nai.renderFile(__dirname + '/hierarchy.html', {
+    //debug: true,
+    locals: {
+      getKeys: function() {
+        return new Promise(function(resolve) {
+          setTimeout(function() {
+            resolve(Object.keys(data));
+          }, Math.random() * 500);
+        });
+      },
+      getValue: function(key) {
+        return new Promise(function(resolve) {
+          setTimeout(function() {
+            resolve(data[key]);
+          }, Math.random() * 1200);
+        });
+      }
+    }
+  })
+  .then(function(result) {
+    console.log('=============== RESULT: ');
+    console.log(result);
+  });
